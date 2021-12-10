361e5f81b30d5572c267509b2d1cd8ba94a0aa63
nkristesiashvili <nkristesiashvili@tbcbank.com.ge>
2021-12-10 20:27:59 +0400

    feat(database) sqlite database init

--- HolidayMailer/LoginPage.cs
+++ HolidayMailer/LoginPage.cs
@@ -2,9 +2,15 @@ namespace HolidayMailer
 {
     public partial class LoginPage : Form
     {
+        List <PersonModel> persons =  new List <PersonModel> ();
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+        private void LoadPeople()
+        {
+            persons = SqliteDataAccess.LoadPeople();
         }
     }
 }
